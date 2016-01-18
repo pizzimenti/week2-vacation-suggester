502513ac72fe88058f4c59a526383782899426ff
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-18 13:05:16 -0800

    add jQuery pseudocode for control flow

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+// control flow conditional for determining appropriate destinations
+
+//question 1 conditional
+if (array has "hot" in it) {
+  $('.cold').show();
+} else if (array has cold in it) {
+  $('.hot').show();
+}
+
+//question 2 conditional
+//question 3 conditional
+//question 4 conditional
+//question 5 conditional
