e7f6dab22d2e153b6c4bbcce9f47357eb68a9b23
Brad Pizzimenti <brad@gennakersystems.com>
2016-01-18 19:48:06 -0800

    create question variables to get form values from DOM

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,24 @@
 $(function() {
   $('#questions form').submit(function(event) {
+    event.preventDefault();
+    var question1 = $("input#question1").val().toLowerCase();
+    var question2 = $("input#question2").val().toLowerCase();
+    var question3 = $("input#question3").val().toLowerCase();
+    var question4 = $("input#question4").val().toLowerCase();
+    var question5 = $("input#question5").val().toLowerCase();
 
 
-
+console.log(question3)
 
 
 // control flow conditional for determining appropriate destinations
 
 //question 1 conditional
-if (array has "hot" in it) {
-  $('.cold').show();
-} else if (array has cold in it) {
-  $('.hot').show();
-}
+// if (array has "hot" in it) {
+//   $('.cold').show();
+// } else if (array has cold in it) {
+//   $('.hot').show();
+// }
 
 //question 2 conditional
 //question 3 conditional
